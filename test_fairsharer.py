43e2b94d1e6837b325457506ae5d6796a39e1b7f
Krabgalasch <40695202+Krabgalasch@users.noreply.github.com>
2023-12-05 21:13:42 +0100

    Update test_fairsharer.py
    tolerances added

--- test_fairsharer.py
+++ test_fairsharer.py
@@ -5,19 +5,19 @@ import numpy as np
 def test_fair_sharer():
     # Test with one iteration
     result = fair_sharer([0, 1000, 800, 0], 1)
-    assert np.allclose(result, np.array([100, 800, 900, 0]))
+    assert np.allclose(result, np.array([100, 800, 900, 0]), atol=1e-5, rtol=1e-5)
 
     # Test with two iterations
     result = fair_sharer([0, 1000, 800, 0], 2)
-    assert np.allclose(result, np.array([100, 890, 720, 90]))
+    assert np.allclose(result, np.array([100, 890, 720, 90]), atol=1e-5, rtol=1e-5)
 
     # Test with negative values
     result = fair_sharer([-200, 500, -300, 100], 1)
-    assert np.allclose(result, np.array([0, 300, 400, -100]))
+    assert np.allclose(result, np.array([0, 300, 400, -100]), atol=1e-5, rtol=1e-5)
 
     # Test with a large number of iterations
     result = fair_sharer([10, 20, 30, 40, 50], 100)
-    assert np.allclose(result, np.array([25, 25, 25, 25, 25]))
+    assert np.allclose(result, np.array([25, 25, 25, 25, 25]), atol=1e-5, rtol=1e-5)
 
 # Führe die Tests aus, wenn die Datei direkt ausgeführt wird
 if __name__ == '__main__':
