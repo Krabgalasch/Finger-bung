a07f539846e92d5d2349ef6f55399f4375fd600f
Krabgalasch <40695202+Krabgalasch@users.noreply.github.com>
2023-12-05 21:06:01 +0100

    Update test_fairsharer.py

--- test_fairsharer.py
+++ test_fairsharer.py
@@ -3,21 +3,21 @@ import pytest
 import numpy as np
 
 def test_fair_sharer():
-    # Test mit einer Iteration
+    # Test with one iteration
     result = fair_sharer([0, 1000, 800, 0], 1)
-    assert np.allclose(result, [100, 800, 900, 0])  # Überprüfe, ob das Ergebnis korrekt ist
+    assert np.allclose(result, np.array([100, 800, 900, 0]))
 
-    # Test mit zwei Iterationen
+    # Test with two iterations
     result = fair_sharer([0, 1000, 800, 0], 2)
-    assert np.allclose(result, [100, 890, 720, 90])  # Überprüfe, ob das Ergebnis korrekt ist
+    assert np.allclose(result, np.array([100, 890, 720, 90]))
 
-    # Test mit negativen Werten
+    # Test with negative values
     result = fair_sharer([-200, 500, -300, 100], 1)
-    assert np.allclose(result, [0, 300, 400, -100])  # Überprüfe, ob das Ergebnis korrekt ist
+    assert np.allclose(result, np.array([0, 300, 400, -100]))
 
-    # Test mit einer großen Anzahl von Iterationen
+    # Test with a large number of iterations
     result = fair_sharer([10, 20, 30, 40, 50], 100)
-    assert np.allclose(result, [25, 25, 25, 25, 25])
+    assert np.allclose(result, np.array([25, 25, 25, 25, 25]))
 
 # Führe die Tests aus, wenn die Datei direkt ausgeführt wird
 if __name__ == '__main__':
